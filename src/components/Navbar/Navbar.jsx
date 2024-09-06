88cc4bf16d43b1f4b8af045c2e59346280fd06f0
tkolsrud <thomaskolsrud@gmail.com>
2024-09-06 04:13:03 -0700

    stubs up sidemenu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import Offcanvas from 'react-bootstrap/Offcanvas'
 
 import * as dummyJSONService from '../../services/dummyJSONService'
 
 function Navbar(){
     const [categories, setCategories] = useState([])
+    const [show, setShow] = useState(false)
 
     useEffect(() => {
         const fetchCategories = async() => {
@@ -14,8 +16,26 @@ function Navbar(){
         fetchCategories()
     }, [])
 
+    const categoryList = categories.map((category) => {
+        return <li>{category.name}</li>
+    })
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
     return (
         <>
+        <button onClick={handleShow}>Departments</button>
+        <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Departments</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <ul>
+                    {categoryList}
+                </ul>
+            </Offcanvas.Body>
+        </Offcanvas>
         <h1><NavLink to='/'>Daintree</NavLink></h1>
         <p><NavLink to='/login'>Login</NavLink></p>
         </>
