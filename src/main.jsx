5f190b7f1288a3a280d1b5bf7303939afe2d9887
tkolsrud <thomaskolsrud@gmail.com>
2024-09-06 03:45:45 -0700

    installs react-bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import './index.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(
